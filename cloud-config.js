4ecc1c50eeb1726d2288f59914118db83515157c
wagoxyz-lab <wagoxyz@gmail.com>
2026-07-14 11:14:49 +0800

    Secure cloud analytics dashboard

---
+++ cloud-config.js
@@ -0,0 +1,5 @@
+window.GAME_CLOUD_CONFIG = Object.freeze({
+  url: 'https://tnlvmqteneggwoblhfcg.supabase.co',
+  publishableKey: 'sb_publishable_q2xizblzYqFt-pNzGOLJ8Q_k_b-98a4',
+  adminEmail: 'demonoflaplace099@gmail.com'
+});
